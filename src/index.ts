71191ca5d89c1563cdaaa23287eb43f48f2b63d0
sunshj <47186980+sunshj@users.noreply.github.com>
2025-03-10 01:51:39 +0800

    feat: support multiple text selections

--- src/index.ts
+++ src/index.ts
@@ -17,46 +17,64 @@ import {
   defineExtension,
   useActiveTextEditor,
   useCommand,
-  useTextEditorSelection
+  useTextEditorSelections
 } from 'reactive-vscode'
 import { commands, displayName } from './generated/meta'
 import { logger } from './utils'
+import type { Selection } from 'vscode'
+
+interface TextSelectionData {
+  index: number
+  text: string
+  selection: Selection
+}
 
 const { activate, deactivate } = defineExtension(() => {
   logger.info(`Extension [${displayName}] activated`)
   const editor = useActiveTextEditor()
   const document = computed(() => editor.value?.document)
-  const selection = useTextEditorSelection(editor)
-  const selectedText = computed(() => document.value?.getText(selection.value) || '')
+  const selections = useTextEditorSelections(editor)
 
-  function replace(newText: string) {
-    if (!selectedText.value.trim()) return
-    editor.value?.edit(editBuilder => {
-      editBuilder.replace(selection.value, newText)
-    })
+  const textSelectionsData = computed<TextSelectionData[]>(() =>
+    selections.value.map((selection, index) => ({
+      index,
+      text: document.value?.getText(selection) || '',
+      selection
+    }))
+  )
+
+  async function replace(newTextSelectionsData: TextSelectionData[]) {
+    for (const { selection, text, index } of textSelectionsData.value) {
+      if (!text.trim()) return
+      await editor.value?.edit(editBuilder => {
+        editBuilder.replace(selection, newTextSelectionsData.find(i => i.index === index)!.text)
+      })
+    }
+  }
+
+  function mapText(converter: (input: string) => string): TextSelectionData[] {
+    return textSelectionsData.value.map(t => ({ ...t, text: converter(t.text) }))
   }
 
   // string case
-  useCommand(commands.pascalCase, () => replace(pascalCase(selectedText.value)))
-  useCommand(commands.camelCase, () => replace(camelCase(selectedText.value)))
-  useCommand(commands.kebabCase, () => replace(kebabCase(selectedText.value)))
-  useCommand(commands.snakeCase, () => replace(snakeCase(selectedText.value)))
-  useCommand(commands.pascalSnakeCase, () => replace(pascalSnakeCase(selectedText.value)))
-  useCommand(commands.trainCase, () => replace(trainCase(selectedText.value)))
-  useCommand(commands.constantCase, () => replace(constantCase(selectedText.value)))
-  useCommand(commands.upperCase, () => replace(selectedText.value.toUpperCase()))
-  useCommand(commands.lowerCase, () => replace(selectedText.value.toLowerCase()))
-  useCommand(commands.capitalCase, () => replace(capitalCase(selectedText.value)))
-  useCommand(commands.sentenceCase, () => replace(sentenceCase(selectedText.value)))
-  useCommand(commands.dotCase, () => replace(dotCase(selectedText.value)))
-  useCommand(commands.pathCase, () => replace(pathCase(selectedText.value)))
-  useCommand(commands.noCase, () => replace(noCase(selectedText.value)))
+  useCommand(commands.pascalCase, () => replace(mapText(pascalCase)))
+  useCommand(commands.camelCase, () => replace(mapText(camelCase)))
+  useCommand(commands.kebabCase, () => replace(mapText(kebabCase)))
+  useCommand(commands.snakeCase, () => replace(mapText(snakeCase)))
+  useCommand(commands.pascalSnakeCase, () => replace(mapText(pascalSnakeCase)))
+  useCommand(commands.trainCase, () => replace(mapText(trainCase)))
+  useCommand(commands.constantCase, () => replace(mapText(constantCase)))
+  useCommand(commands.upperCase, () => replace(mapText(t => t.toUpperCase())))
+  useCommand(commands.lowerCase, () => replace(mapText(t => t.toLowerCase())))
+  useCommand(commands.capitalCase, () => replace(mapText(capitalCase)))
+  useCommand(commands.sentenceCase, () => replace(mapText(sentenceCase)))
+  useCommand(commands.dotCase, () => replace(mapText(dotCase)))
+  useCommand(commands.pathCase, () => replace(mapText(pathCase)))
+  useCommand(commands.noCase, () => replace(mapText(noCase)))
 
   // desensitize
-  useCommand(commands.desensitizeStar, () =>
-    replace(selectedText.value.replaceAll(/[A-Z0-9]/gi, '*'))
-  )
-  useCommand(commands.desensitizeX, () => replace(selectedText.value.replaceAll(/[A-Z0-9]/gi, 'x')))
+  useCommand(commands.desensitizeStar, () => replace(mapText(t => t.replaceAll(/[A-Z0-9]/gi, '*'))))
+  useCommand(commands.desensitizeX, () => replace(mapText(t => t.replaceAll(/[A-Z0-9]/gi, 'x'))))
 })
 
 export { activate, deactivate }
